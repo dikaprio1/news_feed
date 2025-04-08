c10aa031152021da101514c4355c3e4eec43f5a1
J <justonesip82@gmail.com>
2025-04-09 02:37:06 +0900

    fix: 런타임 오류 픽스
    UserRepository 내 불필요한 메서드 제거

--- src/main/java/com/example/news_feed/user/repository/UserRepository.java
+++ src/main/java/com/example/news_feed/user/repository/UserRepository.java
@@ -22,6 +22,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // 이메일로 조회
     Optional<User> findByEmail(String email);
 
-
-    Optional<User> findIdByEmailAndPassword(String e);
 }
