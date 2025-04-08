d0f09d6021ed3296c9ca0790f31d8993833c2063
J <justonesip82@gmail.com>
2025-04-08 23:38:21 +0900

    fix: @notnull 삭제

--- src/main/java/com/example/news_feed/user/dto/UpdateNamePwRequestDto.java
+++ src/main/java/com/example/news_feed/user/dto/UpdateNamePwRequestDto.java
@@ -10,15 +10,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UpdateNamePwRequestDto {
 
-    @NotNull
     private final String username;
-
-    @NotNull
-    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
-            message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다")
     private final String oldPassword;
-
-    @NotNull
     @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
             message = "비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다")
     private final String newPassword;
