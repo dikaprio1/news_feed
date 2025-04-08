68685eefa1723add23ba5f29f0c9e4fed2071567
J <justonesip82@gmail.com>
2025-04-08 15:48:26 +0900

    feat:서비스계층 @Slf4j추가

--- src/main/java/com/example/news_feed/auth/service/AuthServiceImpl.java
+++ src/main/java/com/example/news_feed/auth/service/AuthServiceImpl.java
@@ -5,11 +5,12 @@ import com.example.news_feed.config.PasswordEncoder;
 import com.example.news_feed.user.entity.User;
 import com.example.news_feed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
@@ -23,6 +24,7 @@ public class AuthServiceImpl implements AuthService {
 
         // 이메일 중복체크
         if (userRepository.findByEmail(requestDto.getEmail()).isPresent()) {
+            log.warn("이미 존재하는 사용자입니다 : " + requestDto.getEmail());
             throw new ResponseStatusException(HttpStatus.CONFLICT, "이미 존재하는 사용자입니다"); // 중복시 409
         }
 
