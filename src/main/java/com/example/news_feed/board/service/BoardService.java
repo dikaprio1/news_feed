2c0eb3444306d03b77b9b7fedfb4d00590f5962a
김민준 <minsa05@nate.com>
2025-04-09 10:24:09 +0900

    feat:게시물 생성
    게시물 생성 추가

--- src/main/java/com/example/news_feed/board/service/BoardService.java
+++ src/main/java/com/example/news_feed/board/service/BoardService.java
@@ -1,4 +1,10 @@
 package com.example.news_feed.board.service;
 
+import com.example.news_feed.board.dto.BoardRequestDto;
+import com.example.news_feed.board.dto.BoardResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface BoardService {
+    BoardResponseDto createPosts(BoardRequestDto boardRequestDto);
 }
--- src/main/java/com/example/news_feed/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/news_feed/board/service/BoardServiceImpl.java
@@ -1,4 +1,14 @@
 package com.example.news_feed.board.service;
 
-public class BoardServiceImpl {
+import com.example.news_feed.board.dto.BoardRequestDto;
+import com.example.news_feed.board.dto.BoardResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardServiceImpl implements BoardService{
+
+    @Override
+    public BoardResponseDto createPosts(BoardRequestDto boardRequestDto) {
+        return new BoardResponseDto();
+    }
 }
