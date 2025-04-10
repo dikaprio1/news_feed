cd976d5b86fd88f2759bbf6608e1db7c48d39155
J <justonesip82@gmail.com>
2025-04-10 19:53:06 +0900

    feat: 로그인 필터 구현
    - LoginFilter 생성
    - WebConfig 생성

---
+++ src/main/java/com/example/news_feed/filter/LoginFilter.java
@@ -0,0 +1,47 @@
+package com.example.news_feed.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = {"/", "/api/auth/signup"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        log.info("로그인 필터 로직 실행");
+
+        if (isWhiteList(requestURI)) {
+
+            HttpSession session = httpRequest.getSession(false);
+
+            if (session == null || session.getAttribute("user") == null) {
+                throw new RuntimeException("로그인 해주세요.");
+            }
+
+            log.info("로그인에 성공했습니다.");
+
+        }
+
+        chain.doFilter(request, response);
+
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+
+}
