9b4a90cfe8cff0613301c41d3a39d15e6c1c1ce8
J <justonesip82@gmail.com>
2025-04-13 18:29:39 +0900

    feax: 예외핸들러에 회원가입, 로그인 관련 예외 추가

--- src/main/java/com/example/news_feed/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/news_feed/exceptionhandler/GlobalExceptionHandler.java
@@ -2,8 +2,14 @@ package com.example.news_feed.exceptionhandler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,4 +32,30 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleAllExceptions(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류 발생: " + ex.getMessage());
     }
+
+    // @Valid 관련 오류 처리 -> 메세지만 띄움
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.put(error.getField(), error.getDefaultMessage()); // 필드명 : 메시지
+        }
+
+        return ResponseEntity.badRequest().body(errors); // 400
+    }
+
+    // 중복 회원가입 409
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<String> handleDuplicateUser(DuplicateUserException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    // 로그인 실패 401
+    @ExceptionHandler(InvalidLoginException.class)
+    public ResponseEntity<String> handleInvalidLogin(InvalidLoginException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+
 }
