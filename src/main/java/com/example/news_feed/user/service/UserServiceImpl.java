9fa8229f8cb9c9051a5345734ea0088ee928951b
J <justonesip82@gmail.com>
2025-04-13 19:48:28 +0900

    fix: 유저crud 예외핸들러 적용
    - InvalidUserInputException 적용
    - UnauthorizedAccessException 적용
    - UserNotFoundException 적용
    - 일부 메세지 내용 수정

--- src/main/java/com/example/news_feed/user/service/UserServiceImpl.java
+++ src/main/java/com/example/news_feed/user/service/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.news_feed.user.service;
 
 import com.example.news_feed.config.PasswordEncoder;
+import com.example.news_feed.exceptionhandler.InvalidLoginException;
+import com.example.news_feed.exceptionhandler.InvalidUserInputException;
+import com.example.news_feed.exceptionhandler.UnauthorizedAccessException;
+import com.example.news_feed.exceptionhandler.UserNotFoundException;
 import com.example.news_feed.user.dto.*;
 import com.example.news_feed.user.entity.User;
 import com.example.news_feed.user.repository.UserRepository;
@@ -27,7 +31,7 @@ public class UserServiceImpl implements UserService {
     public UserResponseDto findById(Long id) {
         User findUser = userRepository.findByIdOrElseThrow(id);
         if (findUser.getDeletedAt() != null){
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"이미 탈퇴한 회원입니다"); //400
+            throw new UserNotFoundException("사용자를 찾을 수 없습니다"); // 이미 탈퇴한 회원 404
         }
 
         return new UserResponseDto(findUser.getName(), findUser.getEmail(), findUser.getGender(), findUser.getAge());
@@ -41,19 +45,19 @@ public class UserServiceImpl implements UserService {
         User findUser = userRepository.findByIdOrElseThrow(id); //id 체크
 
         if (!findUser.getEmail().equals(sessionId)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인 계정만 삭제할 수 있습니다."); // 로그인한 회원과 대상 유저 동일한지 확인
+            throw new UnauthorizedAccessException("본인 계정만 수정할 수 있습니다."); // 로그인한 회원과 대상 유저 동일한지 확인
         }
 
         if (!passwordEncoder.matches(requestDto.getOldPassword(), findUser.getPassword())) { // 비번체크
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"); // 401 반환
+            throw new InvalidLoginException("비밀번호가 일치하지 않습니다"); // 401 반환
         }
 
-        if (!requestDto.getUsername().isBlank()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이름을 입력해주세요"); // 변경할 이름 공란일 시 400 반환
+        if (requestDto.getUsername().isBlank()) {
+            throw new InvalidUserInputException("이름을 입력해주세요"); // 변경할 이름 공란일 시 400 반환
         }
 
         if (requestDto.getUsername().equals(findUser.getName())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "기존 이름과 동일합니다");  // 400 반환
+            throw new InvalidUserInputException("기존 이름과 동일합니다");  // 400 반환
         }
         findUser.updateName(requestDto.getUsername());
 
@@ -67,15 +71,15 @@ public class UserServiceImpl implements UserService {
         User findUser = userRepository.findByIdOrElseThrow(id); //id 체크
 
         if (!findUser.getEmail().equals(sessionId)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인 계정만 삭제할 수 있습니다."); // 로그인한 회원과 대상 유저 동일한지 확인
+            throw new UnauthorizedAccessException("본인 계정만 수정할 수 있습니다."); // 로그인한 회원과 대상 유저 동일한지 확인
         }
 
         if (!passwordEncoder.matches(requestDto.getOldPassword(), findUser.getPassword())) { // 비번체크
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"); // 401 반환
+            throw new InvalidLoginException("비밀번호가 일치하지 않습니다"); // 401 반환
         }
 
         if (passwordEncoder.matches(requestDto.getNewPassword(), findUser.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "현재 비밀번호와 동일합니다"); // 400 반환
+            throw new InvalidUserInputException("현재 비밀번호와 동일합니다"); // 400 반환
             }
             findUser.updatePassword(passwordEncoder.encode(requestDto.getNewPassword()));
     }
@@ -88,19 +92,19 @@ public class UserServiceImpl implements UserService {
         User findUser = userRepository.findByIdOrElseThrow(id); // 탈퇴 대상 유저 조회
 
         if (!findUser.getEmail().equals(sessionId)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인 계정만 삭제할 수 있습니다."); // 로그인한 회원과 대상 유저 동일한지 확인
+            throw new UnauthorizedAccessException("본인 계정만 삭제할 수 있습니다."); // 로그인한 회원과 대상 유저 동일한지 확인
         }
 
         if (!findUser.getEmail().equals(requestDto.getEmail())) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다"); // 아이디 불일치 401 반환
+            throw new InvalidLoginException("아이디 또는 비밀번호가 올바르지 않습니다"); // 아이디 불일치 401 반환
         }
 
         if (!passwordEncoder.matches(requestDto.getPassword(), findUser.getPassword())) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "아이디 또는 비밀번호가 올바르지 않습니다"); // 비번 불일치 401 반환
+            throw new InvalidLoginException("아이디 또는 비밀번호가 올바르지 않습니다"); // 비번 불일치 401 반환
         }
 
         if (findUser.getDeletedAt() != null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND); // 이미 탈퇴한 회원 404 반환
+            throw new UserNotFoundException("사용자를 찾을 수 없습니다"); // 이미 탈퇴한 회원 404
         }
 
         LocalDateTime now = LocalDateTime.now();
